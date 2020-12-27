8ec65d52787910a4bdb2dc3093856db90581e3bd
Cal <callum@glovefx.com>
2020-12-27 20:59:52 +0000

    Added empty vertex class

---
+++ include/geometry/vertex.h
@@ -0,0 +1,28 @@
+#ifndef GBOX_VERTEX_H
+#define GBOX_VERTEX_H
+
+#include <iostream>
+
+namespace gbox
+{
+  class Vertex
+  {
+    public:
+      // dtor
+      ~Vertex();
+      // ctor
+      Vertex();
+      // copy ctor
+      Vertex(const Vertex& _vertex_other);
+      // copy assignment operator
+      Vertex& operator=(const Vertex& _vertex_other);
+      // move ctor
+      Vertex(Vertex&& _vertex_other);
+      // move assignment operator
+      Vertex& operator=(Vertex&& _vertex_other);
+
+    private:
+  };// class Vertex
+}// namespace gbox
+
+#endif // GBOXVERTEXH
---
+++ src/geometry/vertex.cpp
@@ -0,0 +1,47 @@
+#include "geometry/vertex.h"
+
+gbox::Vertex::Vertex()
+{
+  #ifdef DEBUG_PRINTS
+  std::cout<<"Ctor called for Vertex @"<<std::hex<<this<<std::dec<<'\n';
+  #endif
+}
+
+gbox::Vertex::~Vertex()
+{
+  #ifdef DEBUG_PRINTS  
+  std::cout<<"Dtor called for Vertex @"<<std::hex<<this<<std::dec<<'\n';
+  #endif
+}
+
+gbox::Vertex::Vertex(const Vertex& _vertex_other)
+{
+  #ifdef DEBUG_PRINTS
+  std::cout<<"Copy ctor called for Vertex and Vertex\n";
+  #endif
+}
+
+gbox::Vertex& gbox::Vertex::operator=(const Vertex& _vertex_other)
+{
+  #ifdef DEBUG_PRINTS
+  std::cout<<"Copy assignment called for Vertex\n";
+  #endif
+  
+  return *this;
+}
+
+gbox::Vertex::Vertex(Vertex&& _vertex_other)
+{
+  #ifdef DEBUG_PRINTS
+  std::cout<<"Move ctor called for Vertex and Vertex\n";
+  #endif
+}
+
+gbox::Vertex& gbox::Vertex::operator=(Vertex&& _vertex_other)
+{
+  #ifdef DEBUG_PRINTS
+  std::cout<<"Move assignment called for Vertex and Vertex\n";
+  #endif
+
+  return *this;
+}
